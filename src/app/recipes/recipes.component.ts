04c32aba92eb14a0562d3ce9e06efbeb8c211728
Sonali Sawlani <sonalisawlani707@gmail.com>
2020-01-07 09:30:59 +0530

    Add files via upload

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes.model';
+import {RecipeService} from './recipe.service';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService]
+})
+export class RecipesComponent implements OnInit {
+
+  selectedRecipe: Recipe;
+
+  constructor() { }
+  ngOnInit() {
+    
+  }
+}
